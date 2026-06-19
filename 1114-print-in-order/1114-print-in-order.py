bb91c0210e194fa41e1dbf5f0a790329c82fb5e6
Vattoli Sai Krishna chaithanya <165250796+vskChaithanya@users.noreply.github.com>
2026-06-19 22:35:41 +0530

    Time: 3374 ms (6.24%), Space: 19.9 MB (48.5%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.py
@@ -0,0 +1,25 @@
+class Foo:
+    def __init__(self):
+        self.first_done=False
+        self.second_done=False
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.first_done=True
+        
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        
+        # printSecond() outputs "second". Do not change or remove this line.
+        while not self.first_done:
+            pass
+        printSecond()
+        self.second_done=True
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        while not self.second_done:
+            pass
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
